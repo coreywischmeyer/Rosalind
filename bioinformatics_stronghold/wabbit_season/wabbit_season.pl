17dba0f7b8ebd3d155022a469c85fec66593a059
unknown <CEWISC@PBM8PR4.na.ds.monsanto.com>
2014-03-24 17:34:20 -0500

    Wabbit Season with mortal rabbits is giving me recursion errors. Will have to figure out why.

--- bioinformatics_stronghold/wabbit_season/wabbit_season.pl
+++ bioinformatics_stronghold/wabbit_season/wabbit_season.pl
@@ -1,19 +1,15 @@
 use strict;
 use warnings;
-
+use Memoize;
+memoize('rabbit_fib');
 my ($months, $age) = ($ARGV[0], $ARGV[1]) or die;
 print rabbit_fib($months, $age) . "\n";
+
 sub rabbit_fib{
   my $n = shift;
-  my $rabbits = shift;
-  return $n if($n == 0 or $n == 1);
-  return rabbit_fib($n-1, $rabbits) + (rabbit_fib($n - 2, $rabbits));
+  my $age = shift;
+  return $n if($n == 0 or $n == 1 or $n == -1);
+  return rabbit_fib($n - 1, $age) 
+  		  	+ rabbit_fib($n - 2, $age)
+  		  	- rabbit_fib($n - $age - 1, $age);
 }
-__END__
-
-1 1 not mature
-2 1 mature
-3 4 have 3 pairs
-4 7 have 3 more
-5 
-
