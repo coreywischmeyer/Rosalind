4d5f88f7cd81e1e70de1b9cef27fac42e9c96dcb
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2014-03-24 20:23:27 -0500

    Finally have a good algorithm for parsing FASTA files. Saved it as a separate gist.

--- bioinformatics_stronghold/gc_content/gc_content.pl
+++ bioinformatics_stronghold/gc_content/gc_content.pl
@@ -4,18 +4,29 @@ use Data::Dumper;
 
 my $fasta_file = shift or die "Where is your FASTA?";
 open(my $fh, "<", $fasta_file);
-$/= ">";
-my $sequence = "";
-my $header = <$fh>;
-while(<$fh>){ 
-	my $sequence = $_ =~ s/\n//g;
-}
-#prin	t Dumper \%sequences;
 
-#print "$max_name\n$max \n";
+$/= ">";
+
+my $header = <$fh>;
+my $sequence;
+my $max_header = "";
+my $max_gc = 0;
+while(<$fh>){
+  chomp;
+  if($_ =~ m/(.+)\n/){
+   $header = $1;
+   s/$header//;
+  }
+  ($sequence = $_) =~  s/\n//g;
+  if((my $curr_gc = get_gc($sequence)) > $max_gc){
+    $max_gc = $curr_gc;
+    $max_header = $header;
+  }
+}
+
+print "$max_header\n$max_gc\n";
 
 sub get_gc{
-	my $name = shift;
 	my $sequence = shift;
 	my $length = length($sequence);
 	my $gc = $sequence =~ tr/GC/GC/;
