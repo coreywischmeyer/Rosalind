c5e963fd7997e6ef377eabb2257e60b77452b22e
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2014-03-25 21:09:23 -0500

    Mendel is really owning me on this one. Need to reveiw basic stats.

--- bioinformatics_stronghold/mendels_first_law/mendels_first.pl
+++ bioinformatics_stronghold/mendels_first_law/mendels_first.pl
@@ -1,4 +1,22 @@
 use strict;
 use warnings;
 
+my $input_file = shift or die("Couldn't find the file");
+
+open(my $fh, "<", $input_file) or die("Could not open the file");
+
+my $line = <$fh>;
+chomp($line);
+my @dataset = split(/ /, $line);
+
+print calculate_outcome(\@dataset);
+
+sub calculate_outcome{
+  my $dataset = shift;
+  my($homozygous, $heterozygous, $homo_recessive) = @{$dataset};
+  my $total = $homozygous + $heterozygous + $homo_recessive;
+  return (p($homozygous, $homozygous, $total) 
+          + p($homozygous, $heterozygous, $total)); 
+          
+}
 
