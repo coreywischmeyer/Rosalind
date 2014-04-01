d36b99bb64e2317c6ed4d65c6db3de5d4eea1fd9
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2014-03-31 20:30:13 -0500

    Counts frequences of words

---
+++ python_village/dictionaries/dictionaries.py
@@ -0,0 +1,16 @@
+import sys
+
+file = sys.argv.pop() or sys.exit()
+
+fh = open(file, 'r')
+
+string = fh.readline()
+dictionary = dict()
+for word in string.split(' '):
+	if word in dictionary:
+		dictionary[word] += 1
+	else:
+		dictionary[word] = 1
+
+for key, value in dictionary.items():
+	print key,value
