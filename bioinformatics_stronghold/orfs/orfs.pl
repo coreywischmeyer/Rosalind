b4e7e796e8866136f0377b326efde93a1d3aa197
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2014-04-06 17:00:12 -0500

    Codons work correctly.
    It seems that the question is a little more in depth than I expected.
    All protein strings need to be generated even if they are contained
    inside another one. I did not account for that. It also means that
    some strings would be repetitive, so putting the answers into a hash
    would be a good idea.

--- bioinformatics_stronghold/orfs/orfs.pl
+++ bioinformatics_stronghold/orfs/orfs.pl
@@ -3,22 +3,26 @@ use warnings;
 use Data::Dumper;
 my $string = shift or die;
 
-my @reading_frames = qw(0 1 2);
+my @reading_frames = qw( 0 1 2 3 4 5 6 );
 my $dna_to_aa = load_dna_codon_table();
 for my $reading_frame (@reading_frames){
   my $aa_string = "";
   my $dna_string = $string;
   my $ORF = 0;
+  $dna_string = substr($dna_string, $reading_frame, -1);
+  print "START: $dna_string \n";
   while($dna_string){
-    print "DNA: $dna_string " . length($dna_string) . "\n";
-    my $codon = substr($dna_string, $reading_frame, ($reading_frame + 3));
-    $dna_string = substr($dna_string, ($reading_frame + 3));
-    if($codon eq "AUG" or $ORF){
+    last if(length($dna_string) < 3);
+    my $codon = substr($dna_string, 0, 3);
+    $dna_string = substr($dna_string, 3);
+    print "RF: $reading_frame Codon: $codon \n";
+    if($codon eq "ATG" or $ORF == 1){
       $ORF = 1;
       my $aa = $dna_to_aa->{$codon};
-      if($aa eq "Stop" or length($dna_string) < 3 ){
-        print "$aa_string \n";
-        last;
+      if($aa eq "Stop"){
+        print "$aa_string\n";
+        $aa_string = "";
+        $ORF = 0;
       }else{
         $aa_string .= $aa; 
       }
