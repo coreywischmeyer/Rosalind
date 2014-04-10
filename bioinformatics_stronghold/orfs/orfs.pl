9e975ea3c1ef2ccbdf8200cf39f75b95f74b3fd1
Corey Wischmeyer <corey.wischmeyer@gmail.com>
2014-04-09 20:14:44 -0500

    Reverse complement is important too.
    After reading someone elses answer it appears that I failed to add
    the reverse complement to the possibility of the ORFs. I also
    forgot I have to start a new aa string EACH time there is a start
    site. This means that everytime I come across a methionine I have to
    start another amino acid string.

--- bioinformatics_stronghold/orfs/orfs.pl
+++ bioinformatics_stronghold/orfs/orfs.pl
@@ -3,28 +3,39 @@ use warnings;
 use Data::Dumper;
 my $string = shift or die;
 
-my @reading_frames = qw( 0 1 2 );
-my $dna_to_aa = load_dna_codon_table();
-for my $reading_frame (@reading_frames){
-  my $aa_string = "";
-  my $dna_string = $string;
-  my $ORF = 0;
-  $dna_string = substr($dna_string, $reading_frame, -1);
-  print "START: $dna_string \n";
-  while($dna_string){
-    last if(length($dna_string) < 3);
-    my $codon = substr($dna_string, 0, 3);
-    $dna_string = substr($dna_string, 3);
-    print "RF: $reading_frame Codon: $codon \n";
-    if($codon eq "ATG" or $ORF == 1){
-      $ORF = 1;
-      my $aa = $dna_to_aa->{$codon};
-      if($aa eq "Stop"){
-        print "$aa_string\n";
-        $aa_string = "";
-        $ORF = 0;
-      }else{
-        $aa_string .= $aa; 
+find_protein_strings($string);
+find_protein_strings(reverse_complement($string));
+
+sub reverse_complement{
+  my $string = shift;
+  $string = reverse($string);
+  $string =~ tr/ATCG/TAGC/;
+  return $string;
+}
+
+sub find_protein_strings{
+  my $string = shift;
+  my @reading_frames = qw( 0 1 2 );
+  my $dna_to_aa = load_dna_codon_table();
+  for my $reading_frame (@reading_frames){
+    my $aa_string = "";
+    my $dna_string = $string;
+    my $ORF = 0;
+    $dna_string = substr($dna_string, $reading_frame, -1);
+    while($dna_string){
+      last if(length($dna_string) < 3);
+      my $codon = substr($dna_string, 0, 3);
+      $dna_string = substr($dna_string, 3);
+      if($codon eq "ATG" or $ORF == 1){
+        $ORF = 1;
+        my $aa = $dna_to_aa->{$codon};
+        if($aa eq "Stop"){
+          print "$aa_string\n";
+          $aa_string = "";
+          $ORF = 0;
+        }else{
+          $aa_string .= $aa; 
+        }
       }
     }
   }
